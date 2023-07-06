57c816f73d2f8aa4f2b0d94355973b98a0b14718
abibrito <abinadabebrito@gmail.com>
2023-07-06 00:56:10 -0300

    botton add

---
+++ src/components/button/button.jsx
@@ -0,0 +1,11 @@
+import style from './button.module.css'
+
+function Button() {
+  return (
+    <div>
+      <button className={style['btn']}>Avançar</button>
+    </div>
+  )
+}
+
+export default Button
