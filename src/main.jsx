d901af6a0129df0c83ed3a80da855df8fa4602de
abibrito <abinadabebrito@gmail.com>
2023-07-07 11:12:32 -0300

    feat: arquivos pagina de finalizar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Postagem from './pages/postagem/postagem'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+import Postagem from "./pages/postagem/postagem";
+import Finish from "./pages/finish/finish";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Postagem />,
+  },
+  {
+    path: "/Finish",
+    element: <Finish />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Postagem />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/finish/finish.jsx
@@ -0,0 +1,9 @@
+import style from "./finish.module.css"
+
+function Finish() {
+  return (
+    <div className={ style[''] }>finish</div>
+  )
+}
+
+export default Finish
