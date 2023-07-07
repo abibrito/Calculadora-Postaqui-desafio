80b3b521cf7e70f9058aff2f62f5b3cd0b1bf414
abibrito <abinadabebrito@gmail.com>
2023-07-07 16:59:41 -0300

    feat: pagina de final criada e assets add

--- src/pages/finish/finish.jsx
+++ src/pages/finish/finish.jsx
@@ -1,9 +1,26 @@
-import style from "./finish.module.css"
+import Header from "../../components/header/header";
+import Correio from "../../assets/img/FinishCorreios.png";
+import Button from "../../components/button/button";
+
+import style from "./finish.module.css";
 
 function Finish() {
   return (
-    <div className={ style[''] }>finish</div>
-  )
+    <div className={style["finish-container"]}>
+      <Header />
+      <div className={style["bk-img"]}>
+        <img src={ Correio } />
+      </div>
+      <div className={style["text-container"]}>
+        <p>Parabéns o seu frete foi postado com sucesso.</p>
+        <span>O seu códico de rastreio é </span>
+        <p>BBBBBBBBBBBBBBBB</p>
+      </div>
+      <div className={style['bloco-btn']}>
+        <Button value={"Nova Postagem"}/>
+      </div>
+    </div>
+  );
 }
 
-export default Finish
+export default Finish;
